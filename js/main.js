49a5b003a7bcf17420df343c12717c1d4b5acd80
Brian Blackwell <industriousthought@gmail.com>
2014-12-29 21:46:38 +0000

    Added a collision rule object.

--- js/main.js
+++ js/main.js
@@ -26,6 +26,8 @@ require.config({
 
 
 require([
+    'js/collisionRule',
+
     'js/utils', 
 
     'player/player',
@@ -36,36 +38,69 @@ require([
 
     'physicsjs/behaviors/edge-collision-detection',
 
-    'physicsjs/integrators/improved-euler',
+    'physicsjs/behaviors/sweep-prune',
 
     'physicsjs/behaviors/body-impulse-response',
 
+    'physicsjs/integrators/improved-euler',
+
     'physicsjs/behaviors/sweep-prune',
 
-    //'physicsjs/behaviors/body-collision-detection',
+    'physicsjs/behaviors/body-collision-detection',
+
+    'physicsjs/behaviors/zombie',
 
     'physicsjs/behaviors/player',
 
     'physicsjs/renderers/canvas',
+
+    'physicsjs/bodies/rectangle', // will mix into the PhysicsJS library
 
     'physicsjs/bodies/circle', // will mix into the PhysicsJS library
 
     'physicsjs/bodies/sprite'
 
 
-    ], function( Utils, Player, Zombies, Physics ){
+    ], function( CollisionRule, Utils, Player, Zombies, Physics ){
 
 
         Physics(function( world ){
-            Utils.setupWorld({init: [Player, Zombies, Utils]}, world, Physics);
+            Utils.setupWorld({init: [CollisionRule, Player, Zombies, Utils]}, world, Physics);
 
-            Zombies.add({x: 90, y: 90, type: 2});
+            Utils.addBackground({ 
+                styles: {
+                    src: 'img/tile.png'
+
+                }, 
+                
+                repeat: { 
+                
+                    columns: 10,
+
+                    rows: 10
+
+                },
+
+                width: 300,
+
+                height: 300
+            });
+
+            //Zombies.add({x: 90, y: 90, type: 2});
+            //Zombies.add({x: 490, y: 490, type: 2});
+
             window.p = Player.add({x: 160, y: 160});
 
-            Utils.follow(p);
-            var controller = Physics.behavior('player', { player: p })
+            //Utils.follow(p);
+            var controller = Physics.behavior('player', { player: p });
             world.add(controller);
         
+            world.add(Physics.behavior('zombie', { zombies: Zombies, follow: p }));
+
+            world.on('collisions:detected', function(e) {
+                CollisionRule.collisions(e.collisions);
+                
+            });
         });
 
     }
